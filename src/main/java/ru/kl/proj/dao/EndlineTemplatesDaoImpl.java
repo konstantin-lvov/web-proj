104d56a5bf44997376da827ad5f38fc5f6724a1b
konstantin-lvov <nattts@yahoo.com>
2020-03-27 15:12:30 +0500

    create endlinetemplate dao impl

---
+++ src/main/java/ru/kl/proj/dao/EndlineTemplatesDaoImpl.java
@@ -0,0 +1,57 @@
+package ru.kl.proj.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import ru.kl.proj.Mappers.EndlineTemplatesMapper;
+import ru.kl.proj.entity.EndlineTemplates;
+
+import java.util.List;
+
+public class EndlineTemplatesDaoImpl implements Dao<EndlineTemplates> {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Override
+    public List<EndlineTemplates> getAll() {
+        String sql = "select * from public.endline_template;";
+        return jdbcTemplate.query(sql, new EndlineTemplatesMapper());
+    }
+
+    @Override
+    public void create(EndlineTemplates entity) {
+        String sql = "insert into public.endline_template " +
+                "(oid, etid, endline_template) values(?, ?, ?);";
+        jdbcTemplate.update(sql,
+                entity.getOid(),
+                entity.getEtid(),
+                entity.getEndlineTemplate());
+    }
+
+    @Override
+    public EndlineTemplates read(String entityProp) {
+        int oid = Integer.parseInt(entityProp);
+        String sql = "select * from public.endline_template " +
+                "where oid = ?;";
+        return jdbcTemplate.queryForObject(sql,
+                new EndlineTemplatesMapper(),
+                oid);
+    }
+
+    @Override
+    public void update(EndlineTemplates entity) {
+        String sql = "update public.endline_template " +
+                "set endline_template = ? where oid = ? and etid = ?;";
+        jdbcTemplate.update(sql,
+                entity.getEndlineTemplate(),
+                entity.getOid(),
+                entity.getEtid());
+    }
+
+    @Override
+    public void delete(String entityProp) {
+        int etid = Integer.parseInt(entityProp);
+        String sql = "delete from public.endline_template where etid = ?;";
+        jdbcTemplate.update(sql,
+                etid);
+    }
+}
