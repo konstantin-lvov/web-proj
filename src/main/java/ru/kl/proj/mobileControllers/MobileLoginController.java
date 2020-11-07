c12d5bd4ec972d280bcc48610882a4e9f8107e4f
konstantin-lvov <nattts@yahoo.com>
2020-11-07 18:56:01 +0500

    not working log4j added, begin JSONBuilder class

--- src/main/java/ru/kl/proj/mobileControllers/MobileLoginController.java
+++ src/main/java/ru/kl/proj/mobileControllers/MobileLoginController.java
@@ -1,5 +1,7 @@
 package ru.kl.proj.mobileControllers;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -8,6 +10,8 @@ import ru.kl.proj.dao.OrganizationDaoImpl;
 import ru.kl.proj.entity.AuthToken;
 import ru.kl.proj.entity.Organization;
 import ru.kl.proj.services.TokenGenerator;
+
+import java.lang.reflect.Field;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
@@ -23,12 +27,16 @@ public class MobileLoginController {
     @Autowired
     AuthToken authToken;
 
+    private static Logger logger = LogManager.getLogger(MobileLoginController.class);
+
     /*
     Входящий запрос имеет поля - имя и пароль
      */
     @RequestMapping(value="/mobileLogin", method=GET)
     public String mobileLogin(@RequestParam(value = "organization", required = true) String organizationName,
                         @RequestParam(value = "password", required = true) String organizationPassword){
+
+        logger.info("Info log message");
 
         TokenGenerator tokenGenerator = new TokenGenerator();
         String existingToken = "";
@@ -38,6 +46,7 @@ public class MobileLoginController {
         try{
             //если организации не существует то процесс авторизации прекращается по исключению
             Organization organization = organizationDao.readByName(organizationName);
+
             oid = organization.getOid();
             tokenExisting = authToken.isExist(oid);
 
